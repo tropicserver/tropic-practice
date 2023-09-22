e516a9b9c0f8f9bb41d114f986955b71f4d96c51
GrowlyX <growlyxdev@gmail.com>
2023-09-22 15:55:03 -0400

    feat: add armor contents and use arrays for kit stuff

--- shared/src/main/kotlin/gg/tropic/practice/kit/Kit.kt
+++ shared/src/main/kotlin/gg/tropic/practice/kit/Kit.kt
@@ -9,14 +9,15 @@ import org.bukkit.inventory.ItemStack
  * @author GrowlyX
  * @since 9/17/2023
  */
-data class Kit(
+class Kit(
     val id: String,
     val displayName: String,
     val displayIcon: ItemStack = ItemBuilder
         .of(Material.POTATO)
         .build(),
     var enabled: Boolean = false,
-    var contents: List<ItemStack> = listOf(),
+    var armorContents: Array<ItemStack?> = arrayOfNulls(4),
+    var contents: Array<ItemStack?> = arrayOfNulls(36),
     var additionalContents: List<ItemStack> = listOf(),
     val features: MutableMap<FeatureFlag, MutableMap<String, String>> = mutableMapOf()
 )
