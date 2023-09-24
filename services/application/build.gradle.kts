df8f344dcc646216aa95d9176aa31d377cdf5032
GrowlyX <growlyxdev@gmail.com>
2023-09-24 13:00:11 -0400

    feat: nicer arg parsing for the ApplicationServer

--- services/application/build.gradle.kts
+++ services/application/build.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     implementation("gg.scala.commons:serversync:3.2.2")
     implementation("gg.scala.commons:serializers:3.2.2")
 
+    implementation("com.xenomachina:kotlin-argparser:2.0.7")
+    implementation("gg.scala.store:shared:0.1.8")
+
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("gg.scala.aware:aware:1.1.9")
     implementation("io.lettuce:lettuce-core:6.2.6.RELEASE")
--- services/application/src/main/kotlin/gg/tropic/practice/application/ApplicationServer.kt
+++ services/application/src/main/kotlin/gg/tropic/practice/application/ApplicationServer.kt
@@ -1,13 +1,68 @@
 package gg.tropic.practice.application
 
+import com.mongodb.MongoClient
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
 import gg.scala.aware.AwareHub
 import gg.scala.aware.uri.WrappedAwareUri
+import gg.scala.cache.uuid.ScalaStoreUuidCache
+import gg.scala.cache.uuid.impl.distribution.DistributedRedisUuidCacheTranslator
+import gg.scala.cache.uuid.resolver.impl.MojangDataResolver
 import gg.scala.commons.agnostic.sync.ServerSync
+import gg.scala.store.ScalaDataStoreShared
+import gg.scala.store.connection.AbstractDataStoreConnection
+import gg.scala.store.connection.mongo.AbstractDataStoreMongoConnection
+import gg.scala.store.connection.mongo.impl.UriDataStoreMongoConnection
+import gg.scala.store.connection.mongo.impl.details.DataStoreMongoConnectionDetails
+import gg.scala.store.connection.redis.AbstractDataStoreRedisConnection
+import gg.tropic.practice.application.api.DPSRedisService
 import gg.tropic.practice.application.api.DPSRedisShared
 import gg.tropic.practice.application.api.defaults.kit.KitDataSync
 import gg.tropic.practice.application.api.defaults.map.MapDataSync
 import gg.tropic.practice.replications.manager.ReplicationManager
 import net.evilblock.cubed.serializers.Serializers
+
+class ApplicationServerArgs(parser: ArgParser)
+{
+    val redisHost by parser
+        .storing(
+            "-rh", "--redishost",
+            help = "The host of the Redis server"
+        )
+        .default("127.0.0.1")
+
+    val redisPort by parser
+        .storing(
+            "-rp", "--redisport",
+            help = "The port of the Redis server"
+        ) {
+            toInt()
+        }
+        .default(6379)
+
+    val mongoHost by parser
+        .storing(
+            "-mh", "--mongohost",
+            help = "The host of the Mongo server"
+        )
+        .default("127.0.0.1")
+
+    val mongoDatabase by parser
+        .storing(
+            "-mdb", "--mongodatabase",
+            help = "The database for the Mongo server"
+        )
+        .default("Scala")
+
+    val mongoPort by parser
+        .storing(
+            "-mp", "--mongoport",
+            help = "The port of the Mongo server"
+        ) {
+            toInt()
+        }
+        .default(27017)
+}
 
 /**
  * @author GrowlyX
@@ -15,10 +70,14 @@ import net.evilblock.cubed.serializers.Serializers
  */
 fun main(args: Array<String>)
 {
+    val parsedArgs = ArgParser(args)
+        .parseInto {
+            ApplicationServerArgs(it)
+        }
     AwareHub.configure(
         WrappedAwareUri(
-            args.getOrNull(0) ?: "localhost",
-            args.getOrNull(1)?.toIntOrNull() ?: 6379
+            parsedArgs.redisHost,
+            parsedArgs.redisPort
         )
     ) {
         Serializers.gson
@@ -26,6 +85,44 @@ fun main(args: Array<String>)
 
     DPSRedisShared.keyValueCache
     ServerSync.configureIndependent()
+
+    val aware = DPSRedisService("dps")
+        .configure { this }
+
+    ScalaDataStoreShared.INSTANCE =
+        object : ScalaDataStoreShared()
+        {
+            override fun getNewRedisConnection(): AbstractDataStoreRedisConnection
+            {
+                return object : AbstractDataStoreRedisConnection()
+                {
+                    override fun createNewConnection() = aware
+                }
+            }
+
+            override fun getNewMongoConnection(): AbstractDataStoreMongoConnection
+            {
+                return UriDataStoreMongoConnection(
+                    DataStoreMongoConnectionDetails(
+                        database = parsedArgs.mongoDatabase
+                    ),
+                    MongoClient(parsedArgs.mongoHost, parsedArgs.mongoPort)
+                )
+            }
+
+            override fun debug(from: String, message: String)
+            {
+                AbstractDataStoreConnection.LOGGER.info("$from: $message")
+            }
+
+            override fun forceDisableRedisThreshold() = true
+        }
+
+
+    ScalaStoreUuidCache.configure(
+        DistributedRedisUuidCacheTranslator(aware),
+        MojangDataResolver
+    )
 
     // TODO: logging? how do we know what the fuck is going on?
     MapDataSync.load()
