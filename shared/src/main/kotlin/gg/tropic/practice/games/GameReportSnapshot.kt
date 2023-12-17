86cfd654887c41c8e96b24ae3bfe0c191306d9fe
GrowlyX <growlyxdev@gmail.com>
2023-12-17 15:44:47 -0500

    fix: fix all potions being counted as health potions

--- shared/src/main/kotlin/gg/tropic/practice/games/GameReportSnapshot.kt
+++ shared/src/main/kotlin/gg/tropic/practice/games/GameReportSnapshot.kt
@@ -1,7 +1,9 @@
 package gg.tropic.practice.games
 
+import com.cryptomorin.xseries.XMaterial
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.inventory.meta.PotionMeta
 
 /**
  * @author GrowlyX
@@ -18,7 +20,8 @@ class GameReportSnapshot(player: Player)
     val healthPotions = player.inventory.contents
         .filterNotNull()
         .count {
-            it.type == Material.POTION
+            it.type == Material.POTION &&
+                it.data.data.toInt() == 16421
         }
 
     val mushroomStews = player.inventory.contents
