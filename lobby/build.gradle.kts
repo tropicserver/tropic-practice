9784ab0022938104abfa09f7efcb3981abedf761
GrowlyX <growlyxdev@gmail.com>
2023-12-24 22:47:47 -0500

    fix: include model libs

--- lobby/build.gradle.kts
+++ lobby/build.gradle.kts
@@ -2,4 +2,7 @@ dependencies {
     api(project(":shared"))
     api(project(":services:queue"))
     api(project(":services:statistics"))
+
+    api(project(":services:games:game-models"))
+    api(project(":services:replications:replication-models"))
 }
